e454546d1a43a0aa1782a1746345cf605bc21469
Teejay Dixon <tahjgdixon@gmail.com>
2022-10-13 15:53:30 -0700

    commit tree c2b80d24cdb78d8fcfd2d2219dc80e87c7c08b1d

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,11 @@
 class Author < ApplicationRecord
+validates_presence_of :name, uniqueness: true
+
+validates_uniqueness_of :name, :case_sensitive => true
+
+validates :phone_number, length: {is: 10}
+
+
+
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  validates_presence_of :title
+
+  validates :content, length: {minimum: 250}
+
+  validates :summary, length: {maximum: 250}
+
+ validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+ validate :clickbate
+
+ Titles = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+
+ def clickbate
+  if Titles.none? {|char| char.match(title)}
+    errors.add(:title, "its not clickbait")
+  end
+ end
+
 end
